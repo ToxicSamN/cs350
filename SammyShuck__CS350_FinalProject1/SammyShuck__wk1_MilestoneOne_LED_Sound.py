2ade6909b2550b29ccd6f63f65e040fecb04cbfe
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2021-01-09 19:29:08 -0700

    Update SammyShuck__wk1_MilestoneOne_LED_Sound.py

--- SammyShuck__CS350_FinalProject1/SammyShuck__wk1_MilestoneOne_LED_Sound.py
+++ SammyShuck__CS350_FinalProject1/SammyShuck__wk1_MilestoneOne_LED_Sound.py
@@ -35,11 +35,11 @@ grovepi.pinMode(led_mid, "OUTPUT")
 grovepi.pinMode(led_hi, "OUTPUT")
 
 # threshold to turn the led_low on 125.00 * 5 / 1024 = 0.61v
-low_thres = 125
+low_thres = 350
 # threshold to turn the led_mid on 400.00 * 5 / 1024 = 1.95v
-mid_thres = 250
+mid_thres = 500
 # threshold to turn the led_hi on 700.00 * 5 / 1024 = 3.41v
-hi_thres = 375
+hi_thres = 650
 
 
 def delay(t=0.01):
