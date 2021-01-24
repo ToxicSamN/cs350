5dc5a6ef7de34ccfcb67fdf366e7ecc1b7804371
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2021-01-24 10:23:15 -0700

    Update SammyShuck__wk4_MilestoneThree_LightSense_LED.py

--- SammyShuck__CS350_FinalProject1/SammyShuck__wk4_MilestoneThree_LightSense_LED.py
+++ SammyShuck__CS350_FinalProject1/SammyShuck__wk4_MilestoneThree_LightSense_LED.py
@@ -502,7 +502,7 @@ if __name__ == "__main__":
                 grovepi.digitalWrite(led, LED.OFF)
 
             print("sensor_value = %d resistance = %.2f" % (sensor_value, K))
-            time.sleep(0.5)  # Sleep for 0.5 seconds
+            time.sleep(0.01)  # Sleep for 0.5 seconds
 
     except BaseException as e:
         print("\nException: Cleaning up")
