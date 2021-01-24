cd3c6a43d3863621cb62d07e76385356bb9fb4bd
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2021-01-24 09:55:22 -0700

    Update SammyShuck__wk4_MilestoneThree_LightSense_LED.py

--- SammyShuck__CS350_FinalProject1/SammyShuck__wk4_MilestoneThree_LightSense_LED.py
+++ SammyShuck__CS350_FinalProject1/SammyShuck__wk4_MilestoneThree_LightSense_LED.py
@@ -492,11 +492,7 @@ if __name__ == "__main__":
             sensor_value = grovepi.analogRead(light_sensor)
 
             # Calculate resistance in K
-            K = float(1023 - sensor_value * 10 / sensor_value)
-            print(1023 - sensor_value)
-            print(1023 - sensor_value * 10)
-            print((1023 - sensor_value * 10) / sensor_value)
-            print(float((1023 - sensor_value * 10) / sensor_value))
+            K = float(1023 - sensor_value * 10.0 / sensor_value)
 
             if K > light_threshold:
                 # turn on LED
